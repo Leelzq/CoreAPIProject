060e075c445932391c66066beda8af119db009aa
lzqTest <lzqTest@qq.com>
2022-01-10 12:08:11 +0800

    测试class

---
+++ GitHubLZQ/CoreAPIProject/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAPIProject
+{
+    public class Class
+    {
+        public void show()
+        {
+
+        }
+    }
+}
